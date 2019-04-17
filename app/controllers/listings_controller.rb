1ac8a2f24ab47affd66fe6158bc403f46c26828d
Bryce harlum <bryce.harlum@gmail.com>
2019-04-17 14:55:34 +1000

    added controllers and views for listings

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,23 @@
+class ListingsController < ApplicationController
+    def create
+        #create new listing
+    end
+    def update
+        #update existing listing
+    end
+    def index
+        #show all listings
+    end
+    def edit
+        #edit an existing listing
+    end
+    def destroy
+        #delete an existing listing
+    end
+    def new
+        #create a new listing
+    end
+    def show
+        #show a single listing
+    end
+end
