7a3593ec4494aa82326cca51d04e14cd11c8b018
Bryce harlum <bryce.harlum@gmail.com>
2019-04-18 12:14:48 +1000

    whitlisted listing params

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,33 +3,44 @@ class ListingsController < ApplicationController
     
     def create
         #create new listing
+        @listing = Listing.create(listing_params)
     end
+
     def update
         #update existing listing
     end
+
     def index
         #show all listings
         @listing = Listings.all
     end
+
     def edit
         #edit an existing listing
     end
+
     def destroy
         #delete an existing listing
     end
+
     def new
         #create a new listing
         @listing = Listing.new
         @breeds = Breed.all
         @sexes = Listing.sexes.keys
     end
+
     def show
         #show a single listing
     end
-
+    
     private
     
     def set_listing
         @listing = Listing.find(params[:id])
+    end
+
+    def listing_params
+        params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet)
     end
 end
