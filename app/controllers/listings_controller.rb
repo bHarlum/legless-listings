2f9d2d75084a0414aae70fc712882db5aa4760bb
Bryce harlum <bryce.harlum@gmail.com>
2019-04-26 10:41:19 +1000

    added in stripe gem and modified show view with deposit button

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,12 @@
 class ListingsController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :set_listing, only: [:show, :edit, :update, :destroy]
+    before_action :authorize_user, only: [:edit, :update, :destroy]
     before_action :set_breeds_and_sexes, only: [:new, :edit]
     
     def create
         #create new listing
-        @listing = Listing.create(listing_params)
+        @listing = current_user.listings.create(listing_params)
         if @listing.errors.any?
             set_breeds_and_sexes
             render("new")
@@ -44,6 +45,12 @@ class ListingsController < ApplicationController
     
     def set_listing
         @listing = Listing.find(params[:id])
+    end
+
+    def authorize_user
+        if @listing.user_id != current_user.id
+            redirect_to listings_path
+        end
     end
 
     def set_breeds_and_sexes
